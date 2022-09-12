d63e3ba0fa2025f2f07877be635976b336ff9442
missstaff <shawnastaff@gmail.com>
2022-09-11 18:08:15 -0700

    emoticon floats up 300 px, fades out and reappears in the emoji bar

--- components/Animation.js
+++ components/Animation.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Animated, Image, TouchableOpacity } from "react-native";
 
 import styles from "../styles/styles";
@@ -6,12 +6,14 @@ import styles from "../styles/styles";
 
 const Animation = (props) => {
 
-  const write = async() => {
+  const write = async () => {
     const writeData = await props.onPress(props.emoji, props.gameId, props.userId);
     return writeData;
   };
 
   const [animation, setAnimation] = useState(new Animated.Value(0));
+  const [fadeAnim, setFadeAnim] = useState(useRef(new Animated.Value(1)).current); 
+
 
   const animationStyles = {
     transform: [
@@ -19,21 +21,28 @@ const Animation = (props) => {
         translateY: animation,
       }
     ]
-  }
+  };
 
   const startAnimation = () => {
     Animated.timing(animation, {
       useNativeDriver: true,
       toValue: -300,
-      duration: 1500,
+      duration: 1000,
     }).start(() => {
-      setAnimation(new Animated.Value(0))
-    });
+      Animated.timing(fadeAnim, {
+        useNativeDriver: true,
+        toValue: 0,
+        duration: 1000
+      }).start(() => {
+        setAnimation(new Animated.Value(0));
+        setFadeAnim(new Animated.Value(1));
+      });
+    })
   }
 
   return (
     <TouchableOpacity style={animationStyles} onPress={() => [write(), startAnimation()]}>
-      <Animated.View>
+      <Animated.View style={{opacity: fadeAnim}}>
         <Image style={[styles.emoji]} source={props.emojiUri} />
       </Animated.View>
     </TouchableOpacity>
